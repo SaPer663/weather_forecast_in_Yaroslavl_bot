dcb4f3d6f979deb2d2860cad2e0297288949393a
SaPer663 <saper663ru@gmail.com>
2021-11-23 21:48:16 +0500

    added dev-dependencies and settings

--- bot.py
+++ bot.py
@@ -1,34 +1,36 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
+from parser import get_filled_dictionary
 
-import cherrypy
 import telebot
-import requests
-from config import token_key
-from parser import get_filled_dictionary, json_file_read, json_file_update
 
+from config import token_key
 
 BOT_TOKEN = token_key
 bot = telebot.TeleBot(BOT_TOKEN)
-dataBase = get_filled_dictionary()
-last_date = next(reversed(dataBase))
-
+DATABASE = get_filled_dictionary()
+last_date = next(reversed(DATABASE))
 
 
 @bot.message_handler(commands=['start'])
 def command_start(message):
-    bot.send_message(message.chat.id, f"Привет, я бот прогноза погоды до {last_date} в Ярославле. \
-    Отправь мне дату в формате день.месяц (02.08) и я дам прогноз погоды")
+    bot.send_message(
+        message.chat.id,
+        (f'Привет, я бот прогноза погоды до {last_date} в Ярославле.'
+         ' Отправь мне дату в формате день.месяц (02.08)'
+         ' и я дам прогноз погоды')
+    )
+
 
 @bot.message_handler(content_types=["text"])
 def repeat_all_messages(message):
-    if message.text in dataBase.keys():
-        result = dataBase[message.text]
-        response = f"{result['data-text']}, температура днём {result['temp_max']}, ночь до {result['temp_min']}"
+    if message.text in DATABASE.keys():
+        result = DATABASE[message.text]
+        response = (f"{result['data-text']},"
+                    " температура днём {result['temp_max']},"
+                    " ночь до {result['temp_min']}")
         bot.send_message(message.chat.id, response)
     else:
         bot.send_message(message.chat.id, 'Введите правильную дату')
-    
+
 
 if __name__ == '__main__':
-     bot.polling(none_stop=True)
+    bot.polling(none_stop=True)
